afcba2ae9b6a2f389b01358f38e0c11257315da4
BugHunter-X <askzenai@gmail.com>
2025-01-16 01:54:39 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,8 @@
+function my_function($param1, $param2 = null) {
+  if ($param2 === null) {
+    // Handle the case where $param2 is not provided
+    $param2 = 'default value';
+  }
+
+  // Process $param1 and $param2
+}
